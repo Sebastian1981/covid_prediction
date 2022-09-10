0fa8f629c7184d870dace5ae24cf0b8f209db5f1
Sebastian Domsch <Sebastian.Domsch@googlemail.com>
2022-09-10 16:28:11 +0200

    update about.md

--- streamlit_app/about.py
+++ streamlit_app/about.py
@@ -17,13 +17,14 @@ def run_project_description_app():
 
     try: # this should work on a local computer
         st.markdown((MARKDOWNPATH / 'about.md').read_text())
-        image = Image.open(IMAGEPATH / 'icu_forecast.png')
-        st.image(image, caption='COVID-19 patients per million in Italy.')
+    except: # this should work for whatever reason when deployed in streamlit cloud (...there is a path problem....)
+        st.markdown(Path('./streamlit_app/markdowns/about.md').read_text())
+
+    image = Image.open(IMAGEPATH / 'icu_forecast.png')
+
+    try: # this should work on a local computer
         st.markdown((MARKDOWNPATH / 'image_description.md').read_text())
         st.markdown((MARKDOWNPATH / 'app_usage.md').read_text())
-    except: # this should work for whatever reason when deployed in streamlit cloud (...there is a path problem....)    
-        st.markdown(Path('./streamlit_app/markdowns/about.md').read_text())
-        image = Image.open(IMAGEPATH / 'icu_forecast.png')
-        st.image(image, caption='COVID-19 patients per million in Italy.')
+    except: # this should work for whatever reason when deployed in streamlit cloud (...there is a path problem....)
         st.markdown(Path('./streamlit_app/markdowns/image_description.md').read_text())
         st.markdown(Path('./streamlit_app/markdowns/app_usage.md').read_text())
