37859462ae2e27408297088cc2956877000e6ec0
Sebastian Domsch <Sebastian.Domsch@googlemail.com>
2022-09-10 16:24:31 +0200

    solve image paths problem..hopefully

--- streamlit_app/about.py
+++ streamlit_app/about.py
@@ -12,17 +12,16 @@ MARKDOWNPATH = Path(rootdir) / 'markdowns'
 
 def run_project_description_app():
 
+    image = Image.open(IMAGEPATH / 'frontpage.png')
+    st.image(image, caption='COVID-19 deaths worldwide in March 2020')
+
     try: # this should work on a local computer
-        image = Image.open(IMAGEPATH / 'frontpage.png')
-        st.image(image, caption='COVID-19 deaths worldwide in March 2020')    
         st.markdown((MARKDOWNPATH / 'about.md').read_text())
         image = Image.open(IMAGEPATH / 'icu_forecast.png')
         st.image(image, caption='COVID-19 patients per million in Italy.')
         st.markdown((MARKDOWNPATH / 'image_description.md').read_text())
         st.markdown((MARKDOWNPATH / 'app_usage.md').read_text())
-    except: # this should work for whatever reason when deployed in streamlit cloud (...there is a path problem....)
-        image = Image.open(IMAGEPATH / 'frontpage.png')
-        st.image(image, caption='COVID-19 deaths worldwide in March 2020')    
+    except: # this should work for whatever reason when deployed in streamlit cloud (...there is a path problem....)    
         st.markdown(Path('./streamlit_app/markdowns/about.md').read_text())
         image = Image.open(IMAGEPATH / 'icu_forecast.png')
         st.image(image, caption='COVID-19 patients per million in Italy.')
