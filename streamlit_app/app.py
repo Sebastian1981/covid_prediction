6b7063023033aa5a973052a7aaaeaae63a8c447e
Sebastian Domsch <Sebastian.Domsch@googlemail.com>
2022-09-10 13:06:54 +0200

    update markdown paths

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -9,6 +9,7 @@ from PIL import Image
 # set paths
 rootdir = os.getcwd()
 IMAGEPATH = Path(rootdir) / 'images'
+MARKDOWNPATH = Path(rootdir) / 'markdowns'
 
 
 def main():
@@ -20,11 +21,11 @@ def main():
         st.header('About this Project')
         image = Image.open(IMAGEPATH / 'frontpage.png')
         st.image(image, caption='COVID-19 deaths worldwide in March 2020')
-        st.markdown((Path(rootdir) / 'about.md').read_text())
+        st.markdown((MARKDOWNPATH / 'about.md').read_text())
         image = Image.open(IMAGEPATH / 'icu_forecast.png')
         st.image(image, caption='COVID-19 patients per million in Italy.')
-        st.markdown((Path(rootdir) / 'image_description.md').read_text())
-        st.markdown((Path(rootdir) / 'app_usage.md').read_text()) 
+        st.markdown((MARKDOWNPATH / 'image_description.md').read_text())
+        st.markdown((MARKDOWNPATH / 'app_usage.md').read_text()) 
     elif choice == 'Predict ICU Patients':
         st.header('Explore Live Data from "Our World in Data"')
         run_eda_app()
