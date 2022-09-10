9ca50a346acc46c2a8f429ecb0911635b1b6d638
Sebastian Domsch <Sebastian.Domsch@googlemail.com>
2022-09-10 16:13:22 +0200

    update about.md

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -19,28 +19,16 @@ def main():
     if choice == 'About this Project':
         st.header('About this Project')
         
-        try: # this should work on a local computer
-            image = Image.open(IMAGEPATH / 'frontpage.png')
-            st.image(image, caption='COVID-19 deaths worldwide in March 2020')    
-            st.markdown((MARKDOWNPATH / 'about.md').read_text())
-            image = Image.open(IMAGEPATH / 'icu_forecast.png')
-            st.image(image, caption='COVID-19 patients per million in Italy.')
-            #st.markdown(Path('./streamlit_app/markdowns/image_description.md').read_text())
-            st.markdown((MARKDOWNPATH / 'image_description.md').read_text())
-            #st.markdown(Path('./streamlit_app/markdowns/app_usage.md').read_text())
-            st.markdown((MARKDOWNPATH / 'app_usage.md').read_text())
-        except: # this should work for whatever reason when deployed in streamlit cloud (...there is a path problem....)
-            image = Image.open(IMAGEPATH / 'frontpage.png')
-            st.image(image, caption='COVID-19 deaths worldwide in March 2020')    
-            st.markdown(Path('./streamlit_app/markdowns/about.md').read_text())
-            #st.markdown((MARKDOWNPATH / 'about.md').read_text())
-            image = Image.open(IMAGEPATH / 'icu_forecast.png')
-            st.image(image, caption='COVID-19 patients per million in Italy.')
-            st.markdown(Path('./streamlit_app/markdowns/image_description.md').read_text())
-            #st.markdown((MARKDOWNPATH / 'image_description.md').read_text())
-            st.markdown(Path('./streamlit_app/markdowns/app_usage.md').read_text())
-            #st.markdown((MARKDOWNPATH / 'app_usage.md').read_text())
 
+        image = Image.open(IMAGEPATH / 'frontpage.png')
+        st.image(image, caption='COVID-19 deaths worldwide in March 2020')    
+        st.markdown((MARKDOWNPATH / 'about.md').read_text())
+        image = Image.open(IMAGEPATH / 'icu_forecast.png')
+        st.image(image, caption='COVID-19 patients per million in Italy.')
+        #st.markdown(Path('./streamlit_app/markdowns/image_description.md').read_text())
+        st.markdown((MARKDOWNPATH / 'image_description.md').read_text())
+        #st.markdown(Path('./streamlit_app/markdowns/app_usage.md').read_text())
+        st.markdown((MARKDOWNPATH / 'app_usage.md').read_text())
         
          
     elif choice == 'Predict ICU Patients':
